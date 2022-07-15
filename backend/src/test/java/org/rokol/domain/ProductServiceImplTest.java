ae64a745c063c7537f09433de63ace7165349696
NHDrizzt <biel_s.carvalho2@hotmail.com>
2022-07-15 11:40:59 -0300

    showproduct test

--- backend/src/test/java/org/rokol/domain/ProductServiceImplTest.java
+++ backend/src/test/java/org/rokol/domain/ProductServiceImplTest.java
@@ -38,6 +38,6 @@ class ProductServiceImplTest {
 
     @Test
     void getMarketAndStack() {
- 
+
     }
 }
--- backend/src/test/java/resources/ShowProductResourceTest.java
+++ backend/src/test/java/resources/ShowProductResourceTest.java
@@ -3,6 +3,7 @@ package resources;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.rokol.domain.usecase.ProductServiceImpl;
 import org.rokol.domain.model.FilterProduct;
 import org.rokol.domain.model.Product;
@@ -10,11 +11,13 @@ import org.rokol.domain.ports.api.ProductService;
 import org.rokol.quarkus.adapter.repository.ProductRepositoryInMemoryFactory;
 import org.rokol.quarkus.adapter.repository.model.AbstractDaoFactory;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 @QuarkusTest
 class ShowProductResourceTest {
@@ -23,17 +26,21 @@ class ShowProductResourceTest {
 
     @BeforeEach
     void init(){
-        AbstractDaoFactory abstractDaoFactoryMemory = new ProductRepositoryInMemoryFactory();
-        productService = new ProductServiceImpl(abstractDaoFactoryMemory);
+        productService = mock(ProductServiceImpl.class);
         filterProduct = new FilterProduct();
     }
 
     @Test
     void should_list_products() {
+        Set<Product> setProd = new LinkedHashSet<>();
+        setProd.add(new Product(1, "Test", "Gubee Test"));
+
+        Mockito.when(productService.getMarketAndStack(filterProduct.getMarket(), filterProduct.getStack())).thenReturn(setProd);
+
         Set<Product> myList = productService.getMarketAndStack(filterProduct.getMarket(), filterProduct.getStack());
 
         assertNotNull(myList);
-        assertEquals(3, myList.size());
+        assertEquals(1, myList.size());
         assertFalse(myList.isEmpty());
     }
 }
